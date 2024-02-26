1a7637a594233d3ef3c588e993e5b4dfc89a35e4
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-26 01:55:17 +0000

    implement messages object and sendMsg function to write messages to other players within the game. messages obj is passed to sendMsg function to determine the keypress that corresponds to each message. calls sendMsg function inside handleUserInput function

--- input.js
+++ input.js
@@ -16,14 +16,41 @@ const handleUserInput = (key) => {
     process.exit();
   }
   if (key === "w") {
-    connection.write('Move: up');
-  } else if (key === 'a') {
-    connection.write('Move: left');
-  } else if (key === 's') {
-    connection.write('Move: down');
-  } else if (key === 'd') {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
     connection.write("Move: right");
   }
+
+  sendMsg(messages, key);
 };
+
+const messages = {
+  g: 'I kik u butt',
+  h: 'My snek will kill u snek',
+  j: 'Ha! U ded.',
+  k: 'U loser!',
+  l: 'Y u so bad at snek?',
+}
+
+const sendMsg = (msgObj, key) => {
+  let prop = key
+  if (prop in msgObj) {
+    connection.write(`Say: ${msgObj[prop]}`)
+  }
+}
+
+// const sendMsg = (key) => {
+//   if (key === 'h') {
+//     connection.write('Hi friend!')
+//   } else if (key === 'g') {
+//     connection.write('Get ready to lose!')
+//   }
+// }
+
+
 
 module.exports = {setupInput};
