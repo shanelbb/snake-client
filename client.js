f6a1f10c4afe07f13c10d4b144221327e09ef8c3
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-27 02:39:56 +0000

    setup user initials to be taken as an argument from process.argv array.

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 const net = require("net");
 const { IP, PORT } = require('./constants')
+// array to hold the initials given as an argument upon connecting to server
+const initials = process.argv.slice(2)
 
 // establishes a connection with the game server
 const connect = function () {
@@ -16,7 +18,13 @@ const connect = function () {
   // net connection event to send messages to server
   conn.on("connect", () => {
     conn.write("Successfully connected to game server");
-    conn.write('Name: STM');
+    // Add initials variable to the game space.
+    if (!initials.length) {
+      // if no initials are given, default user name to FRN
+      conn.write(`Name: FRN`);
+    } else {
+      conn.write(`Name: ${initials[0]}`);
+    }
   });
 
   // interpret incoming data as text
